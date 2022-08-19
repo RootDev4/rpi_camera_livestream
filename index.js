ad1e955c878f399b230ea4d21af8d478ab35a912
Chris <rootdev4@gmail.com>
2022-08-19 12:28:47 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-const express = require('express')
-const app = express();
-const fs = require('fs')
-const port = 3000;
-
-// start capture
-const videoStream = require('./videoStream');
-videoStream.acceptConnections(app, {
-        width: 1280,
-        height: 720,
-        fps: 16,
-        encoding: 'JPEG',
-        quality: 7 // lower is faster, less quality
-    }, 
-    '/stream.mjpg', true);
-
-app.use(express.static(__dirname+'/public'));
-app.listen(port, () => console.log(`Example app listening on port ${port}! In your web browser, navigate to http://<IP_ADDRESS_OF_THIS_SERVER>:3000`));
+const LiveStream = require('./lib/rpi-cam')
+module.exports = new LiveStream()
